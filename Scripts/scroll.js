b471c1fba80d43073bffebc1e345cd54e57eb965
Youssef Elkomy <115145731+YoussefElkome@users.noreply.github.com>
2023-09-20 23:00:05 +0300

    fix scrolling

--- Scripts/scroll.js
+++ Scripts/scroll.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const moreButton = document.querySelector('.intro .more-button');
     const featuresSection = document.getElementById('features-section');
-    function smoothScrollTo(targetElement, duration) {
-        const targetPosition = targetElement.getBoundingClientRect().top;
+
+    function smoothScrollTo(targetElement, duration, offset) {
+        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
         const startPosition = window.pageYOffset;
         const startTime = performance.now();
+
         function animate(currentTime) {
             const elapsedTime = currentTime - startTime;
             const progress = Math.min(elapsedTime / duration, 1);
             const easeInOutQuad = progress < 0.5 ? 2 * progress * progress : -1 + (4 - 2 * progress) * progress;
 
-            window.scrollTo(0, startPosition + targetPosition * easeInOutQuad);
+            window.scrollTo(0, startPosition + (targetPosition - startPosition - offset) * easeInOutQuad);
 
             if (progress < 1) {
                 requestAnimationFrame(animate);
@@ -19,7 +21,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         requestAnimationFrame(animate);
     }
+
     moreButton.addEventListener('click', function () {
-        smoothScrollTo(featuresSection, 1000);
+        smoothScrollTo(featuresSection, 1000, -60); // 100px offset
     });
 });
