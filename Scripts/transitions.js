95f540df8dbf747c911d0d2294689616cbbcea45
Youssef Elkomy <115145731+YoussefElkome@users.noreply.github.com>
2023-09-21 00:39:55 +0300

    Transitions Updates

--- Scripts/transitions.js
+++ Scripts/transitions.js
@@ -1,25 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get references to the ".feature" elements within the "Features" section
-    const featureElements = document.querySelectorAll('.container .features');
+    // Get references to the "Features" and "Vications" sections
+    const featuresSection = document.getElementById('features-section');
+    const vacationSection = document.getElementById('vacation-section');
 
-    // Function to handle the intersection of the ".feature" elements
-    function handleFeatureIntersection(entries, observer) {
+    // Get references to the elements to animate in both sections
+    const featuresElementsToAnimate = document.querySelectorAll('.container .features');
+    const vacationElementsToAnimate = document.querySelectorAll('.container .vications');
+
+    function handleIntersection(entries, observer) {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
-                // When a ".feature" element is in the viewport, set opacity to 1
-                entry.target.style.opacity = '1';
-                entry.target.style.transform = 'translateY(0)';
+                // Check which section is intersecting and apply animations accordingly
+                if (entry.target === featuresSection) {
+                    // When the "Features" section enters the viewport, set opacity to 1 and transform to 0
+                    featuresElementsToAnimate.forEach((element) => {
+                        element.style.opacity = 1;
+                        element.style.transform = 'translateY(0)';
+                    });
+                } else if (entry.target === vacationSection) {
+                    // When the "Vications" section enters the viewport, set opacity to 1 and transform to 0
+                    vacationElementsToAnimate.forEach((element) => {
+                        element.style.opacity = 1;
+                        element.style.transform = 'translateY(0)';
+                    });
+                }
+
+                // Stop observing once the animation is applied
+                observer.unobserve(entry.target);
             }
         });
     }
 
     // Create an Intersection Observer
-    const featureObserver = new IntersectionObserver(handleFeatureIntersection, {
+    const intersectionObserver = new IntersectionObserver(handleIntersection, {
         threshold: 0.2, // Adjust this threshold as needed
     });
 
-    // Observe each ".feature" element
-    featureElements.forEach((element) => {
-        featureObserver.observe(element);
-    });
+    // Observe both the "Features" and "Vications" sections
+    intersectionObserver.observe(featuresSection);
+    intersectionObserver.observe(vacationSection);
 });
