33feb8546819c7bfeb44f139d984ff332b42635f
Youssef Elkomy <115145731+YoussefElkome@users.noreply.github.com>
2023-09-21 07:52:55 +0300

    Add animations

---
+++ Scripts/show.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".container .intro");
+    setTimeout(() => {
+        container.classList.add("show");
+    },); // Increase the delay if needed
+});
