17aea9ae68998ce2b226dec55c965048bb79f37e
Youssef Elkomy <115145731+YoussefElkome@users.noreply.github.com>
2023-09-23 16:30:18 +0300

    Fixing

--- Scripts/transitions.js
+++ Scripts/transitions.js
@@ -1,30 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get references to the "Features" and "Vications" sections
     const featuresSection = document.getElementById('features-section');
     const vacationSection = document.getElementById('vacation-section');
     const viewsSection = document.getElementById('views-section');
-    // Get references to the elements to animate in both sections
     const featuresElementsToAnimate = document.querySelectorAll('.container .features');
     const vacationElementsToAnimate = document.querySelectorAll('.container .vications');
     const whatAboutVacations = document.querySelectorAll('.container .what-about-vacations');
     function handleIntersection(entries, observer) {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
-                // Check which section is intersecting and apply animations accordingly
                 if (entry.target === featuresSection) {
-                    // When the "Features" section enters the viewport, set opacity to 1 and transform to 0
                     featuresElementsToAnimate.forEach((element) => {
                         element.style.opacity = 1;
                         element.style.transform = 'translateY(0)';
                     });
                 } else if (entry.target === vacationSection) {
-                    // When the "Vications" section enters the viewport, set opacity to 1 and transform to 0
                     vacationElementsToAnimate.forEach((element) => {
                         element.style.opacity = 1;
                         element.style.transform = 'translateY(0)';
                     });
                 } else if (entry.target === viewsSection) {
-                    // When the "Vications" section enters the viewport, set opacity to 1 and transform to 0
                     whatAboutVacations.forEach((element) => {
                         element.style.opacity = 1;
                         element.style.transform = 'translateY(0)';
@@ -32,18 +26,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
 
 
-                // Stop observing once the animation is applied
                 observer.unobserve(entry.target);
             }
         });
     }
 
-    // Create an Intersection Observer
     const intersectionObserver = new IntersectionObserver(handleIntersection, {
         threshold: 0.2, // Adjust this threshold as needed
     });
 
-    // Observe both the "Features" and "Vications" sections
     intersectionObserver.observe(featuresSection);
     intersectionObserver.observe(vacationSection);
     intersectionObserver.observe(viewsSection);
