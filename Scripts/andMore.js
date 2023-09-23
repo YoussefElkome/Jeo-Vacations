543e0c926080bdaa9e8a1468e43e6ce118d13cf4
Youssef Elkomy <115145731+YoussefElkome@users.noreply.github.com>
2023-09-23 16:28:19 +0300

    and More

--- Scripts/andMore.js
+++ Scripts/andMore.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const andMore = document.getElementById("andMore");
+    setTimeout(() => {
+        andMore.textContent = 'And More.';
+    }, 100); 
+    setTimeout(() => {
+        andMore.textContent = 'And More..';
+    }, 100); 
+    setTimeout(() => {
+        andMore.textContent = 'And More...';
+    }, 100); 
 })
